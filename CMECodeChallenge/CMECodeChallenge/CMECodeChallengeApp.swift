76723d2d4acf445a654081be4bd99cad83e83c8e
Amr Moheb <Amr.Moheb@mdi.com.eg>
2025-01-16 09:47:39 +0200

    First commit: setting up the project

---
+++ CMECodeChallenge/CMECodeChallenge/CMECodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMECodeChallengeApp.swift
+//  CMECodeChallenge
+//
+//  Created by Amr Moheb on 16/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CMECodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CMECodeChallenge/CMECodeChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CMECodeChallenge
+//
+//  Created by Amr Moheb on 16/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
